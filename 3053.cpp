5d64d41f98a3cff8fbf743590f441ecdfcb9f773
MINHA-J <64896960+MINHA-J@users.noreply.github.com>
2021-07-16 16:47:01 +0900

    21.07.16
    3053

---
+++ 3053.cpp
@@ -0,0 +1,29 @@
+// 9. 기본 수학2 - 10단계
+// 택시 기하학
+
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>;
+
+using namespace std;
+
+int main() {
+
+	int R;
+	cin >> R;
+
+	double euclid = R * R * M_PI;
+	cout << fixed;
+	cout.precision(6);
+
+	cout << euclid << "\n";
+
+	double taxi = R * R * 2;
+	cout << fixed;
+	cout.precision(6);
+
+	cout << taxi;
+}
+
+// 맞았습니다!!
